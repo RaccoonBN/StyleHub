09c2e62edd869107497ffbdeece69900f9f7676a
BaoNgocnetroi <tranhuynhbaongoc.job@gmail.com>
2024-10-24 12:50:29 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       {/* Nội dung trang chủ */}
       <div style={{ padding: '80px', color: 'black' }}>
         <h1>Chào mừng đến với StyleHub!</h1>
-        <p>Khám phá những sản phẩm thời trang hàng hiệu từ Nike, Adidas, và nhiều thương hiệu nổi tiếng khác!</p>
+        <p>Khám phá những sản phẩm thời trang hàng hiệu từ Nike, Adidas và nhiều thương hiệu nổi tiếng khác!</p>
       </div>
     {/* Hiển thị sản phẩm */}
     <h2>TẤT CẢ SẢN PHẨM</h2>
